2d24dcb0890017e1142bcd3e986c98c99e939f5f
gav1306 <gavk30@gmail.com>
2024-10-28 16:54:43 +0530

    first commit

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,42 @@
+import { useCallback, useState } from "react";
+import QUESTIONS from "../questions";
+import Question from "./Question";
+import Summary from "./Summary";
+
+const Quiz = () => {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const selectAnswerHandler = useCallback(function selectAnswerHandler(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => selectAnswerHandler(null),
+    [selectAnswerHandler]
+  );
+
+  if (quizIsComplete) {
+    return <Summary userAnswers={userAnswers} />;
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSkipAnswer={handleSkipAnswer}
+        onSelectAnswer={selectAnswerHandler}
+      />
+    </div>
+  );
+};
+
+export default Quiz;
